f49d40740f333984fafc257561a29d411de738b9
Gavin Killough <gavin.killough@salesfox.ai>
2021-03-02 20:17:48 -0500

    SRCH-9: Begin implementing news-search functionality

--- flask-backend/main.py
+++ flask-backend/main.py
@@ -9,19 +9,14 @@ def react_entrypoint():
     return flask.render_template("index.html")
 
 
-"""
-To use this API:
-http://localhost:5000/api/news?zip_code=12345
-"""
-
-
+# To use this API:
+# http://localhost:5000/api/news?zip_code=12345
 @app.route("/api/news")
 def get_news():
     requested_zip_code = flask.request.args.get('zip_code')
     if requested_zip_code is None:
         flask.abort(400, "The 'zip_code' query param is required")
-    zip_code = int(requested_zip_code)
-    return news.retrieve_news(zip_code)
+    return news.retrieve_news(requested_zip_code)
 
 
 app.run(debug=True)
--- flask-backend/news.py
+++ flask-backend/news.py
@@ -1,25 +1,49 @@
-def retrieve_news(zip_code):
-    print(f"Zip Code: {zip_code}")
+import requests
+from env_config import BING_API_KEY
+
+BING_NEWS_SEARCH_URL = "https://api.bing.microsoft.com/v7.0/news/search"
+
+FRESHNESS_DAY = "Day"
+FRESHNESS_WEEK = "Week"
+FRESHNESS_MONTH = "Month"
+
+
+def retrieve_news(search_term):
+    print(f"A search for ['{search_term}'] was performed")
+    bing_news = retrieve_bing_news(search_term, 5)
+
+    salesfox_news = []
+    for news_item in bing_news.get("value"):
+        salesfox_news_item = create_salesfox_news_item(news_item)
+        salesfox_news.append(salesfox_news_item)
+
     return {
-        "total": 3,
-        "items": [
-            {
-                "title": "Example News Headline",
-                "description": "Some news happened today and it was newsworthy news.",
-                "link": "https://salesfox.ai",
-                "source": "Salesfox News"
-            },
-            {
-                "title": "A Different News Headline",
-                "description": "Different news happened recently. Different, but interesting.",
-                "link": "https://salesfox.ai",
-                "source": "Channel Pi News"
-            },
-            {
-                "title": "One Last News Headline",
-                "description": 'This is the last of the news. As they say, "no news is good news."',
-                "link": "https://salesfox.ai",
-                "source": "Fake News Network, Inc."
-            }
-        ]
+        "count": len(salesfox_news),
+        "items": salesfox_news
     }
+
+
+def retrieve_bing_news(search_term, max_results, freshness=FRESHNESS_WEEK):
+    headers = {
+        "Ocp-Apim-Subscription-Key": BING_API_KEY
+    }
+    params = {
+        "q": search_term,
+        "count": max_results,
+        "freshness": freshness,
+        "safeSearch": "Strict",
+        "cc": "US"
+    }
+    response = requests.get(url=BING_NEWS_SEARCH_URL, params=params, headers=headers)
+    return response.json()
+
+
+def create_salesfox_news_item(external_news_item):
+    news_item_providers = external_news_item.get("provider")
+    news_sources = ''.join(map(lambda item: item.get("name"), news_item_providers))
+    return {
+                "title": external_news_item.get("name"),
+                "description": external_news_item.get("description"),
+                "link": external_news_item.get("url"),
+                "source": news_sources
+            }
