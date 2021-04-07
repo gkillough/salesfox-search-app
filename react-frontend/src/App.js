368c64b383d39aeed19f438f2b19dd1b7f1325db
Gavin Killough <gavin.killough@salesfox.ai>
2021-04-06 23:02:45 -0400

    Add Google Analytics

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
-import Header from 'modules/components/Header/header'
-import Home from 'modules/pages/Home/home'
+import Header from 'modules/components/Header/header';
+import Home from 'modules/pages/Home/home';
+import ReactGA from 'react-ga';
+
 function App() {
+  ReactGA.initialize('G-1DH4G83WZL');
   return (
     <div className="App">
     <Header />
